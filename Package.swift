a1e4a759bcdc559f82f6a6daf7b89f07a35e6631
kojirou <kojirouhtc@gmail.com>
2019-12-21 18:08:42 +0800

    update

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["Executable"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", .branch("master"))
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", .revision("edc19d30a674cb9f3311b77ffb406dc7c5d2f540"))
     ],
     targets: [
         .target(
